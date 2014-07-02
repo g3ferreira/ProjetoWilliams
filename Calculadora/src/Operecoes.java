e0b74680b002bc3fa846a511a4a2b09486c11e02
williams <wswilliamssilva938@gmail.com>
2014-07-02 14:51:48 -0400

    quarto commit teste

--- Calculadora/src/Operecoes.java
+++ Calculadora/src/Operecoes.java
@@ -7,7 +7,9 @@ public class Operecoes {
 	public void resultadoSoma(OperacaodeSoma soma,int numero1, int numero2){
 		
 		int result = soma.somaNumeros(numero1, numero2);
-		JOptionPane.showMessageDialog(null, "Resultado da Soma e:  "+result);
+		JOptionPane.showMessageDialog(null, "Resultado da Soma e:aa  "+result);
+		
+		
 
 	}
 
